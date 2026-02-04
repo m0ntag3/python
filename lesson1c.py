d3e70d4b21c7da40493ebef240a575a5b82b0e57
m0ntag3 <kuminacollins@gmail.com>
2026-02-04 13:59:26 +0300

    added lesson 1c

---
+++ lesson1c.py
@@ -0,0 +1,39 @@
+# Arithmetic operations
+# Addition
+number1 = 10
+number2 = 20
+addition = number1 + number2
+print("The sum of the two numbers is", addition)
+
+# Subtration
+number3 = 50
+number4 = 15
+difference = number3 - number4
+print("The difference of the two numbers is", difference)
+
+# Multiplication
+number5 = 10
+number6 = 6
+product = number5 * number6
+print("The product of this two numbers is", product)
+
+# Division
+number7 = 49
+number8 = 7
+division = number7 / number8
+print("the division of this numbers is", division)
+
+# Modulus - Remainder of a number when subdivided by another one.
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print("The modulus of the numbers is", modulus)
+
+# Floor division (//) - Number of times a number subdivides another ignoring the remainder
+floor_division = number9 // number10
+print("The floor division of the two numbers is",floor_division)
+
+# Exponent (**) - Power of a number(A number squared)
+number11 = 10
+exponent = number11 ** 2
+print("The exponent of the number is", exponent)
