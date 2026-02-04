fbb509d206583918fbd539bfb2a91943fde4cb51
m0ntag3 <kuminacollins@gmail.com>
2026-02-04 13:22:34 +0300

    added lesson 1b

---
+++ lesson1b.py
@@ -0,0 +1,18 @@
+# Python Data Types
+# String - Set/series of characters that are inside quotes
+name = "Moses Wachira"
+print(name)
+print(type(name))
+
+# Numeric
+# Int - Indicates the whole number in python, does not use quotes.
+age = 21
+print('The age of this person is:',age)
+print(type(age))
+
+# Float - Number with decimal.
+coordinates = 15.4524
+print("The coordinates are",coordinates)
+print(type(coordinates))
+
+# Complex - Math equations with imaginary numbers.
