b3be3978f0bc8af4ab6e101bd764dd29dbf83bc1
m0ntag3 <kuminacollins@gmail.com>
2026-02-09 09:04:37 +0300

    finished assignment

--- assignmet3.py
+++ assignmet3.py
@@ -19,3 +19,32 @@ elif Gross_Salary >= 50000 and Gross_Salary <= 99999:
     print("Monthly Contribution", 1500.00)
 else:
     print("Monthly Contribution", 2000.00)
+
+
+    # Looping
+    # 1. For loop
+    # 2. While Loop
+
+# For Loop - used to iterate over a sequence.(For every item in collection do "this")
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(f"I love {fruit}s!")
+    # Can either use: i. range(n)
+    #                ii. sequence
+
+    # Range()
+    # To print 0 through 4:
+for i in range(5):
+    print(f"Iteration{i}")
+
+# While loop - used to keep code running as long as a specific condition is True.
+count = 5
+while count > 0:
+    print(f"Countdown: {count}")
+    count -= 1  # Crucial! Without this, you get an infinite loop.
+print("Blast off!")  
+
+# Control statements;
+# 1. Break - Stops loop immediately
+# 2. continue - Skip the rest of the current iteration and jumps to the next.
+# 3. else - run a block of code once the loop finishes normally.
